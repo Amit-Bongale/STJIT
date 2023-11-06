8af9020100bc7d665a1ae5ebc03db237387f9117
Amit-Bongale <amitbongale14@gmail.com>
2023-11-06 13:15:15 +0530

    alt

--- src/components/Branches.js
+++ src/components/Branches.js
@@ -19,32 +19,32 @@ function Branches(){
             <div className='department-container'> 
 
                 <div className='departments'>
-                    <img className='dept-img' src={cse}></img>
+                    <img className='dept-img' src={cse} alt='cse'></img>
                     <h2 className='dept-text'>Computer Science</h2>
                 </div>
 
                 <div className='departments'>
-                    <img className='dept-img' src={ise}></img>
+                    <img className='dept-img' src={ise} alt='ise'></img>
                     <h2 className='dept-text'>Information Science</h2>
                 </div>
 
                 <div className='departments' >
-                    <img className='dept-img' src={ece} ></img>
+                    <img className='dept-img' src={ece} alt='ece'></img>
                     <h2 className='dept-text'>Electronics & Communication</h2>
                 </div>
 
                 <div className='departments'>
-                    <img className='dept-img' src={eee}></img>
+                    <img className='dept-img' src={eee} alt='eee'></img>
                     <h2 className='dept-text'>Electrical & Electronics</h2>
                 </div>
 
                 <div className='departments'>
-                    <img className='dept-img' src={civil}></img>
+                    <img className='dept-img' src={civil} alt='civil'></img>
                     <h2 className='dept-text'>Civil</h2>
                 </div>
 
                 <div className='departments'>
-                    <img className='dept-img' src={mech}></img>
+                    <img className='dept-img' src={mech} alt='mech'></img>
                     <h2 className='dept-text'>Mechanical</h2>
                 </div>
                 
--- src/components/Mainimg.js
+++ src/components/Mainimg.js
@@ -7,7 +7,7 @@ function Mainimg(){
     return(
         <div className='clg-pic'>
             <h1 className='main-text'>Empowering Minds, Inspiring Futures</h1>
-            <img src={clgimg} className='MainImage'></img>
+            <img src={clgimg} className='MainImage' alt='clgimg'></img>
         </div>
     );
 }
