c086146635557733c7fcad80d7c0192434ba040e
Amit-Bongale <amitbongale14@gmail.com>
2023-11-06 13:22:16 +0530

    ph add

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,7 +3,7 @@ import '../App.css'
 function Details(){
     return(
         <div className='headder'>
-        <h5 className='headder-text' >08373-266343, 262075</h5>
+        <h5 className='headder-text' >Ph: 08373-266343, 262075</h5>
         <h5 className='headder-text'> Fax:08373-266427 </h5>
         <h5 className='headder-text'>  CET CODE : E033</h5>
 
